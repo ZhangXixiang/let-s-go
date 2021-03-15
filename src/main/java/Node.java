89d0325a764186e2f1ba42dc70503fdb3d10132e
Tiger <zhangxixiang@imdada.cn>
2021-03-15 16:08:24 +0800

    tree search

---
+++ src/main/java/Node.java
@@ -0,0 +1,113 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Node {
+    char data;
+    Node leftChild;
+    Node rightChild;
+
+    public Node(char data, Node leftChild, Node rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+
+
+    // 前序 根左右
+    // 前序遍历的递推公式：
+    // preOrder(r) = print r->preOrder(r->left)->preOrder(r->right)
+
+    public static void preOrder(Node root){
+        System.out.print(root.data);
+        if(null != root.leftChild){
+            preOrder(root.leftChild);
+        }
+        if(null != root.rightChild) {
+            preOrder(root.rightChild);
+        }
+    }
+
+    // 中序 左跟右
+    // 中序遍历的递推公式：
+    // inOrder(r) = inOrder(r->left)->print r->inOrder(r->right)
+    public static void inOrder(Node root){
+        if(null != root.leftChild){
+            inOrder(root.leftChild);
+        }
+        System.out.print(root.data);
+        if(null != root.rightChild) {
+            inOrder(root.rightChild);
+        }
+    }
+
+    // 后序 左右根
+    // 后序遍历的递推公式：
+    // postOrder(r) = postOrder(r->left)->postOrder(r->right)->print r
+    public static void postOrder(Node root){
+        if(null != root.leftChild){
+            postOrder(root.leftChild);
+        }
+        if(null != root.rightChild) {
+            postOrder(root.rightChild);
+        }
+        System.out.print(root.data);
+    }
+
+
+
+    //深度优先遍历
+    public static void depthFirstSearch(Node rNode) {
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(rNode);
+        Node node = null;
+        while (!stack.empty()) {
+            node = stack.pop();
+            System.out.print(node.data);//遍历根结点
+            if (node.rightChild != null) {
+                stack.push(node.rightChild);//先将右子树压栈
+            }
+            if (node.leftChild != null) {
+                stack.push(node.leftChild);//再将左子树压栈
+            }
+        }
+    }
+
+    //广度优先遍历
+    public static void breadFirstSearch(Node rNode) {
+        LinkedList<Node> queue = new LinkedList<Node>();
+        queue.offer(rNode);
+        Node node = null;
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            System.out.print(node.data);//遍历根结点
+            if (node.leftChild != null) {
+                queue.offer(node.leftChild);//先将左子树入队
+            }
+            if (node.rightChild != null) {
+                queue.offer(node.rightChild);//再将右子树入队
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Node D = new Node('D',null,null);
+        Node E = new Node('E',null,null);
+        Node F = new Node('F',null,null);
+        Node G = new Node('G',null,null);
+        Node B = new Node('B',D,E);
+        Node C = new Node('C',F,G);
+        Node A = new Node('A',B,C);
+
+        // breadFirstSearch(A);
+        // depthFirstSearch(A);
+        // preOrder(A);
+        // inOrder(A);
+        postOrder(A);
+
+    }
+
+
+
+}
---
+++ src/main/java/SortTest.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SortTest {
+
+    public static void main(String[] args) {
+
+        int[] a = {1,2,2,2,2,2,7,8,9};
+        // System.out.println(bsearch(a,9,2));
+        // new StopWatch("a");
+        // 10是初始大小，0.75是装载因子，true是表示按照访问时间排序
+        HashMap<Integer, Integer> m = new LinkedHashMap<>(10, 0.75f, true);
+        m.put(3, 11);
+        m.put(1, 12);
+        m.put(5, 23);
+        m.put(2, 22);
+
+        m.put(3, 26);
+        m.get(5);
+
+        for (Map.Entry e : m.entrySet()) {
+            System.out.println(e.getKey());
+        }
+    }
+
+
+
+    public static int bsearch(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid =  low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == 0) || (a[mid - 1] != value)) return mid;
+                else high = mid - 1;
+                // return mid;
+            }
+        }
+        return -1;
+    }
+}
