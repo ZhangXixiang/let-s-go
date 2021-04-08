6293858875a3be7e38d36192a4435fe9c0b0b975
Tiger <zhangxixiang@imdada.cn>
2021-04-08 15:53:00 +0800

    add template strategy

---
+++ src/main/java/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+
+    private String name;
+
+    private Integer age;
+
+    private Float score;
+
+    public Student(String name, Integer age, Float score) {
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Float getScore() {
+        return score;
+    }
+
+    public void setScore(Float score) {
+        this.score = score;
+    }
+}
